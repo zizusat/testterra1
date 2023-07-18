104d5d6e19763a7ba993304156a2e4fd78d48ab8
satyavara90 <S.Varadarajan2@shell.com>
2023-07-18 19:16:44 +0530

    16 commit

--- variables.tf
+++ variables.tf
@@ -5,7 +5,8 @@ data "azurerm_resource_group" "appgrp2" {
 
 data "azurerm_storage_account" "appstore566565637ss" {
   name                     = "appstore55952111"
- 
+  resource_group_name      = "${data.azurerm_resource_group.appgrp2.name}"
+  
 }
 
 resource "azurerm_storage_container" "data" {
