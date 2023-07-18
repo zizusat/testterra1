fdb6b30a875820cb06f65d7d0a77d2781dd1994e
satyavara90 <S.Varadarajan2@shell.com>
2023-07-18 18:56:52 +0530

    10 commit

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
-data "azurerm_resource_group" "testapp" {
-  name     = "testapp-grp"
-  
+resource "azurerm_resource_group" "appgrp2" {
+  name     = "app-grp"
+  location = "North Europe"
 }
-resource "azurerm_storage_account" "appstorage" {
-  name                     = "testsatya"
-  resource_group_name      = azurerm_resource_group.testapp.name
-  location                 = azurerm_resource_group.testapp.location
+
+resource "azurerm_storage_account" "appstore566565637" {
+  name                     = "appstore566565637"
+  resource_group_name      = "app-grp"
+  location                 = "North Europe"
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
 }
