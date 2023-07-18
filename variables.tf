ddc0b8d1f7072f49d4661ea3aef65fe3a11cd7f1
satyavara90 <S.Varadarajan2@shell.com>
2023-07-18 18:46:51 +0530

    7 commit

--- variables.tf
+++ variables.tf
@@ -1,4 +1,10 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "testapp" {
   name     = "testapp-grp"
   location = "West Europe"
 }
+resource "azurerm_storage_account" "appstorage" {
+  name                     = "testsatya"
+  resource_group_name      = azurerm_resource_group.testapp.name
+  location                 = azurerm_resource_group.testapp.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
