ecd19cb134af1cd159465866fec4963411e46e7b
satyavara90 <S.Varadarajan2@shell.com>
2023-07-19 16:00:07 +0530

    92 commit

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
-data "azurerm_resource_group" "appgrp2" {
-  name     = "SatyaMi"
+resource "azurerm_resource_group" "appgrp2" {
+  name     = "SatyaMi2"
   location = "East US"
   
 }
 
 resource "azurerm_storage_account" "appstore566565637ss" {
     name                     = "appstore566565637"
-  resource_group_name      = "SatyaMi"
+  resource_group_name      = "SatyaMi2"
   location                 = "North Europe"
   account_tier             = "Standard"
   account_replication_type = "LRS"
